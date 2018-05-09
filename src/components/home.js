c27bd8a232f53b7effd2bfef0dcb1c01b71cd64e
Codjam <jamaltbutler>
2018-05-09 19:46:04 -0400

    redo for home and modal design

--- src/components/home.js
+++ src/components/home.js
@@ -1,53 +1,48 @@
-import React, { Component } from 'react';
-import { Tabs, Tab } from 'react'
-import { Grid, Cell } from 'react-mdl'
+import React, {Component} from 'react';
+import {Tabs, Tab} from 'react'
+import {Grid, Cell} from 'react-mdl'
+import { findDOMNode } from 'react-dom';
+import $ from 'jquery';
 
 class Home extends Component {
 
-state = {
-  teas: [ {
-         "id": 1,
-         "name": "Super Interpolation",
-         "description": "Linden Leaf, Elderberry, Burdock Root & Key Lime",
-         "image": "https://github-production-user-asset-6210df.s3.amazonaws.com/31421883/39390080-cbc533c2-4a5d-11e8-9b70-1853f8972d80.png",
-         "price": 10.00
-        },
-        {
-         "id": 2,
-         "name": "Mineral Script",
-         "description": "Sarsaparilla, Stinging Netle & Red Clover",
-         "image": "https://github-production-user-asset-6210df.s3.amazonaws.com/31421883/39390080-cbc533c2-4a5d-11e8-9b70-1853f8972d80.png",
-         "price": 10.00
-       },
-       {
-         "id": 3,
-         "name": "Bootcamp Groove",
-         "description": "Damiana, Ginger, Sensitiva & Yohimbe",
-         "image": "https://github-production-user-asset-6210df.s3.amazonaws.com/31421883/39390080-cbc533c2-4a5d-11e8-9b70-1853f8972d80.png",
-         "price": 10.00
+  state = {
+    teas: [
+      {
+        "id": 1,
+        "name": "Super Interpolation",
+        "description": "Linden Leaf, Elderberry, Burdock Root & Key Lime",
+        "image": "https://github-production-user-asset-6210df.s3.amazonaws.com/31421883/39390080-cbc533c2-4a5d-11e8-9b70-1853f8972d80.png",
+        "price": 10.00
+      }, {
+        "id": 2,
+        "name": "Mineral Script",
+        "description": "Sarsaparilla, Stinging Netle & Red Clover",
+        "image": "https://github-production-user-asset-6210df.s3.amazonaws.com/31421883/39390080-cbc533c2-4a5d-11e8-9b70-1853f8972d80.png",
+        "price": 10.00
+      }, {
+        "id": 3,
+        "name": "Bootcamp Groove",
+        "description": "Damiana, Ginger, Sensitiva & Yohimbe",
+        "image": "https://github-production-user-asset-6210df.s3.amazonaws.com/31421883/39390080-cbc533c2-4a5d-11e8-9b70-1853f8972d80.png",
+        "price": 10.00
 
-       }
+      }
 
-   ]
-};
+    ]
+  }
+
+  componentDidMount() {
+
+  }
 
   render() {
-    return(
-      <div className="home-body">
-        <Grid className="homme-grid">
-          <Cell col={12}>half page</Cell>
-        </Grid>
-        {
-
-          this.state.teas.map(tea => {
-            return
-            <Tabs>
-            <div> {tea.name}{tea.image}{tea.description}{tea.price} </div>
-          </Tabs>
-          })
-        }
-      </div>
-    )
+    return (
+    <div className="home-body">
+      <Grid className="home-grid">
+        <Cell col={12}><h1 className="zoomIn animated font-effect-shadow-multiple">ZINEDTEA</h1></Cell>
+      </Grid>
+    </div>)
   }
 }
 
